3f4aabeb21c91d37dea726e520520e0802ff79dd
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-08-12 20:23:10 +0100

    hgvchgcfcg

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+// the below code is considered middleware. Middleware is a function, that can modify the incoming request data.
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to the endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
